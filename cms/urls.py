1415edca4e52ea151093be4fe6ac1630bd72f531
LocalAdmin <localadmin@administrators-macbook-pro-2.local>
2013-02-05 23:27:47 +0100

    TinyMCE added and used in admin panel

--- cms/urls.py
+++ cms/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,5 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
+	 url(r'^tiny_mce/(?P<path>.*)$', 'django.views.static.serve',
+	 	{ 'document_root' : '/Users/localadmin/projects/cms/tiny_mce/' }),
 	 url(r'', include('django.contrib.flatpages.urls')),
 )
