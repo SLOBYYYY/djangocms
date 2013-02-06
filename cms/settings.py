9e9a4a0cd0d2f104e02814fdb482cb0d3855f2a4
LocalAdmin <localadmin@Administrators-MacBook-Pro-2.local>
2013-02-07 00:19:51 +0100

    Added keywords functionality. On page 43

--- cms/settings.py
+++ cms/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
 	'django.contrib.flatpages',
+	'search',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
